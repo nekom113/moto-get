0d05a7f0b319d97adad756487a45b43d71aa795e
Alex Piontek <nekom113@mail.ru>
2024-08-04 21:18:39 +0300

    add validation

--- js/mail.js
+++ js/mail.js
@@ -1,34 +1,95 @@
 "use strict";
 
-const mailBodyForm = document.querySelector('php-email-body-form');
+const mailBodyForm = document.querySelector('form.php-email-body-form');
+const clientName = mailBodyForm.querySelector('#client-name')
 
-console.log({mailBodyForm});
+// add mask to tel input and validate form
+const im = new Inputmask("+7(999) 999-99-99")
+im.mask(mailBodyForm.querySelector('#client-tel'));
 
-mailBodyForm.addEventListener('submit', e => {
+
+const validation = new JustValidate('#email-body-form')
+
+validation
+  .addField('#client-name', [
+    {
+      rule: "required",
+      errorMessage: "Поле обязательно для заполнения",
+    },
+    {
+      rule: "minLength",
+      value: 2,
+      errorMessage: "Имя слишком короткое",
+    },
+  ])
+  .addField('#client-tel', [
+    {
+      rule: "required",
+      errorMessage: "Поле обязательно для заполнения",
+    },
+    {
+      rule: "minLength",
+      value: 11,
+      errorMessage: "Номер телефона указан неверно",
+    },
+  ]).onSuccess(async function () {
+    const loader = mailBodyForm.querySelector('.body-mail-form-loading')
+    loader.style.display = 'block';
+  let data = {
+        name: clientName.value,
+        tel:mailBodyForm.querySelector('#client-tel').value,
+        promoCode:mailBodyForm.querySelector('#promo_id').value,
+        message:mailBodyForm.querySelector('#message').value,
+  }
+
+  let response = await fetch("https://www.motodc.ru/forms/contact.php", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8"
+    }
+  })
+
+  const result = await response.text()
+  console.log({result})
+  const messageSentBlock = mailBodyForm.querySelector(".mailBodyForm")
+  messageSentBlock.style.display = 'block';
+  setTimeout(() => {
+    messageSentBlock.style.display = 'none';
+   }, 3000)
+})
+
+ 
+ 
+
+
+
+
+
+// add action to button form
+
+// mailBodyForm.querySelector('.email-body-form-button').addEventListener('click', e => {
+// mailBodyForm.addEventListener('click', e => {
+//   e.preventDefault();
+//   e.stopPropagation();
+//   const loader = mailBodyForm.querySelector('.body-mail-form-loading')
+//   loader.style.display = 'block';
+//   setTimeout(() => {
+//     loader.style.display = 'none';
+//    }, 3000)
+
+
+//     console.log({mailBodyFormClientData});
+// })
+
+const mailBodyContaier = document.querySelector('.email-body-form-container')
+document.querySelector('.post-form-activate-icon').addEventListener('click', e => {
   e.preventDefault();
   e.stopPropagation();
-  alert('Сообщение отправлено');
-  const name = mailBodyForm.querySelector('#name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-  const body = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
-  // const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${body}`;
-  const url = '../mail.php';
-
-  async function sendMessage() {
-     const response = await fetch(url, {
-    method: 'POST',
-    body: body
-  })
-  // .then(() => {
-  //   alert('Сообщение отправлено');
-  //   document.getElementById('name').value = '';
-  //   document.getElementById('email').value = '';
-  //   document.getElementById('message').value = '';
-  // }).catch(() => {
-  //   alert('При отправке произошла ошибка');
-  // });
-
-  }
-  sendMessage();
+  mailBodyContaier.style.display = 'flex';
+})
+document.querySelector('.post-form-activate-label').addEventListener('click', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  mailBodyContaier.style.display = 'none';
 })
